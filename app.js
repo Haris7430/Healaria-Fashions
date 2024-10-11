3d5ad900d58b1c0b183f570c252acd5ceface589
Haris7430 <mon986344@gmail.com>
2024-10-11 16:08:17 +0530

    project initialize

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const  express= require('express');
+const app= express();
+const env= require('dotenv').config()
+const db=require('./config/db')
+db()
+
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`server running http://localhost${process.env.PORT}`);
+})
+
+
+module.exports= app
